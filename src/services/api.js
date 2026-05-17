82687127dbb81cbf63cb3c2ce20b788430c8af10
Shiven Sharma <shiven676@gmail.com>
2026-05-17 11:38:50 +0530

    Changed code for vercel deployment

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const apiHost = window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1'
   ? '/'
-  : `${window.location.protocol}//${window.location.hostname}:5000/`;
+  : (window.location.protocol === 'https:' ? '/' : 'http://flowboard.centralindia.cloudapp.azure.com:5000/');
 
 const api = axios.create({
   baseURL: apiHost,
